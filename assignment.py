09fb2381a802b6451a04c92c847947f558df79cb
Tiffany177 <tiffwainaina12@gmail.com>
2025-03-21 17:38:22 +0100

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,9 +1,3 @@
-# Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).
-# Perform the operation based on the user's input and print the result.
-# Example: If a user inputs 10, 5, and +, your program should display 10 + 5 = 15.
-# Note: Upload the code to GitHub and submit the GitHub link
-
-
 print("Welcome to Tiffany's Calculator.")
 
 number1 = float(input("Enter the first number: "))
